fabcc8b1b53f478635bccf3fccaf21fdaff32c4a
Filipe Fernandes <ocefpaf@gmail.com>
2019-01-28 16:54:22 -0200

    sync

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 
+chmod -R +w .
+autoreconf -vfi
+
 export CC=${PREFIX}/bin/h4cc
 export DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib
-export CFLAGS="-fPIC"
+export CFLAGS="-fPIC $CFLAGS"
 
 ./configure --prefix=${PREFIX} \
+            --build=${BUILD} \
+            --host=${HOST} \
             --with-hdf4=${PREFIX} \
             --with-zlib=${PREFIX} \
             --with-jpeg=${PREFIX} \
             --enable-install-include
 
-make
+make -j${CPU_COUNT}
 make check
 make install
 
 pushd include
 make install-includeHEADERS
 popd
-
-# We can remove this when we start using the new conda-build.
-find $PREFIX -name '*.la' -delete
